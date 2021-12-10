73d1bd3aed04122ecf5cfa5b79310b002d527281
HQAQ <1401651730@qq.com>
2021-12-10 18:05:21 +0800

    DFS开始学习

---
+++ demo/src/main/java/com/example/demo/InterviewQuestions/Question0401FindWhetherExistsPath.java
@@ -0,0 +1,16 @@
+package com.example.demo.InterviewQuestions;
+
+/**
+ * @program: demo
+ * @description: 面试题 04.01. 节点间通路
+ * @author: Mr.He
+ * @create: 2021-12-10 14:58
+ **/
+public class Question0401FindWhetherExistsPath {
+
+    public boolean findWhetherExistsPath(int n, int[][] graph, int start, int target) {
+
+        return false;
+    }
+
+}
