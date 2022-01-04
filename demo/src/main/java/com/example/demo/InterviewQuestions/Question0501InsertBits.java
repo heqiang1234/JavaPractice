06b0beac5450452abf9ba04191697d4688318fa4
heqiang1234 <1401651730@qq.com>
2022-01-04 23:06:19 +0800

    位运算

---
+++ demo/src/main/java/com/example/demo/InterviewQuestions/Question0501InsertBits.java
@@ -0,0 +1,26 @@
+package com.example.demo.InterviewQuestions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author HQ
+ * @program: demo
+ * @description: 面试题 05.01. 插入
+ * @date 2022-01-04 21:30:36
+ */
+public class Question0501InsertBits {
+
+    public static long insertBits(int N, int M, int i, int j) {
+        int left = N>>
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //System.out.println(insertBits(1143207437 ,1017033,11,31));
+        System.out.println(insertBits(1024 ,19,2,6));
+    }
+
+}
