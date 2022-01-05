46d8290dea07766bcf1b371d4202991684251387
HQAQ <1401651730@qq.com>
2022-01-05 22:07:32 +0800

    待解决，进位法

--- demo/src/main/java/com/example/demo/InterviewQuestions/Question0501InsertBits.java
+++ demo/src/main/java/com/example/demo/InterviewQuestions/Question0501InsertBits.java
@@ -12,15 +12,20 @@ import java.util.Scanner;
 public class Question0501InsertBits {
 
     public static long insertBits(int N, int M, int i, int j) {
-        int left = N>>
+//        int left = N>>j>>1;//截取左边的部分
+//        left = left<<j<<1; //高位要保持一致
+//        int right = M<<i;
+//        int mid =
 
         return 0;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        //System.out.println(insertBits(1143207437 ,1017033,11,31));
-        System.out.println(insertBits(1024 ,19,2,6));
+//        Scanner scanner = new Scanner(System.in);
+//        //System.out.println(insertBits(1143207437 ,1017033,11,31));
+//        System.out.println(insertBits(1024 ,19,2,6));
+        System.out.println(1<<5);
+        System.out.println((1<<5)-1);
     }
 
 }
---
+++ demo/src/main/java/com/example/demo/InterviewQuestions/Question0503ReverseBits.java
@@ -0,0 +1,10 @@
+package com.example.demo.InterviewQuestions;
+
+/**
+ * @program: demo
+ * @description: 面试题 05.03. 翻转数位
+ * @author: Mr.He
+ * @create: 2022-01-05 22:05
+ **/
+public class Question0503ReverseBits {
+}
