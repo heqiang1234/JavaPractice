a10e4628491522c7c3e578d8c9f495aa57980144
HQAQ <1401651730@qq.com>
2022-01-12 20:03:38 +0800

    dfs和贪心

---
+++ demo/src/main/java/com/example/demo/LeetCode/Question1036IsEscapePossible.java
@@ -0,0 +1,25 @@
+package com.example.demo.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @program: demo
+ * @description: 1036. 逃离大迷宫
+ * @author: Mr.He
+ * @create: 2022-01-11 10:13
+ **/
+public class Question1036IsEscapePossible {
+
+    public int [][] map;
+
+    public boolean isEscapePossible(int[][] blocked, int[] source, int[] target) {
+       // Arrays[m]
+
+        return false;
+    }
+
+
+
+
+
+}
