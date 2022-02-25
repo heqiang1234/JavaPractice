45baee509d1638e241d7bce9cd14ff8d3c183da1
HQAQ <1401651730@qq.com>
2022-02-25 13:48:02 +0800

    单一元素

---
+++ demo/src/main/java/com/example/demo/LeetCode/Question688KnightProbability.java
@@ -0,0 +1,21 @@
+package com.example.demo.LeetCode;
+
+/**
+ * @program: demo
+ * @description: 688. 骑士在棋盘上的概率
+ * @author: Mr.He
+ * @create: 2022-02-17 09:57
+ **/
+public class Question688KnightProbability {
+
+    public static double knightProbability(int n, int k, int row, int column) {
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(knightProbability(3, 2, 0, 0));
+    }
+
+}
