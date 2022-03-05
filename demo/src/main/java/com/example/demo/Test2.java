3cf463d4996f686470c60975c511ecc602ac1419
HQAQ <1401651730@qq.com>
2022-03-05 19:41:25 +0800

    签到 + 未完成

---
+++ demo/src/main/java/com/example/demo/Test2.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+/**
+ * @program: demo
+ * @description: s
+ * @author: Mr.He
+ * @create: 2022-03-04 17:31
+ **/
+public class Test2 {
+
+    final static public String s = "2";
+
+    public static void main(String[] args) {
+        //s = "3";
+    }
+}
