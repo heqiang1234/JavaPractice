252e1bd27de271240bb89a219c375c4a78efed73
HQAQ <1401651730@qq.com>
2022-03-22 14:12:54 +0800

    hash

--- demo/src/main/java/com/example/demo/LeetCode/Question720LongestWord.java
+++ demo/src/main/java/com/example/demo/LeetCode/Question720LongestWord.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.apache.coyote.http11.Constants.a;
-
 /**
  * @program: demo
  * @description: 720. 词典中最长的单词
@@ -15,21 +13,38 @@ import static org.apache.coyote.http11.Constants.a;
 public class Question720LongestWord {
 
     public static String Q(String[] words) {
-        Arrays.sort(words, (a,b) ->{
-            if (a.length() != b.length()){
-                return a.length() - b.length();
-            } else {
-                return b.compareTo(a);
-            }
-        });
+//        Arrays.sort(words, (a,b) ->{
+//            if (a.length() != b.length()){
+//                return a.length() - b.length();
+//            } else {
+//                return b.compareTo(a);
+//            }
+//        });
+//        (o1, o2) -> o2 - o1)
+
+       // Arrays.sort(words,(o1, o2) -> o2.length() - o1.length() );
+       // sort(words,0,words.length - 1);
         String res = "";
         Set<String> set = new HashSet<>();
-        set.add("");
-        int n = words.length;
-        for (int i = 0; i < n; i++) {
-            String str = words[i];
-            if (set.contains(str.substring(0, str.length() - 1))){
-                set.add(str);
+        for (int i = 0; i < words.length; i++) {
+            set.add(words[i]);
+        }
+        for (String str: words) {
+            int len1 = str.length();
+            int len2 = res.length();
+            if (len1 < len2){
+                continue;
+            }
+            if (len1 == len2 && str.compareTo(res) > 0){
+                continue;
+            }
+            Boolean flag = true;
+            for (int i = 0; i < str.length(); i++) {
+               if (!set.contains(str.substring(0,str.length() - i))){
+                    flag = false;
+                }
+            }
+            if (flag){
                 res = str;
             }
         }
@@ -37,7 +52,7 @@ public class Question720LongestWord {
     }
 
     public static void main(String[] args) {
-        String []res = {"w","wo","wor","worl", "world"};
+        String []res = {"a", "banana", "app", "appl", "ap", "apply", "apple"};
         System.out.println(Q(res));
     }
 }
